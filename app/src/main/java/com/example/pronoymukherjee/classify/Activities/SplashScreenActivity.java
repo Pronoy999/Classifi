71890e1da82942210019f27b05ef967a80baa093
Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>
2018-03-17 20:38:53 +0530

    Added the Activities.

---
+++ app/src/main/java/com/example/pronoymukherjee/classify/Activities/SplashScreenActivity.java
@@ -0,0 +1,15 @@
+package com.example.pronoymukherjee.classify.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.pronoymukherjee.classify.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
