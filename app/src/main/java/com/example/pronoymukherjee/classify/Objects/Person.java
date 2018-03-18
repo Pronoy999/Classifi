2769a74974051e1381212f2e5b1659dccc1934ab
Debanik <debanik96@gmail.com>
2018-03-18 12:30:08 +0530

    Added course adding/removing functionality to teacher.

--- app/src/main/java/com/example/pronoymukherjee/classify/Objects/Person.java
+++ app/src/main/java/com/example/pronoymukherjee/classify/Objects/Person.java
@@ -1,21 +1,35 @@
 package com.example.pronoymukherjee.classify.Objects;
+import java.sql.Array;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Person base class for a generic user on the site.
  */
 
 abstract class Person {
-    private String name, department, email, phoneNumber, yearOfJoin, collegeCode;
-    private Date dob;
-    private char gender;
+    protected String ID;
+    protected String name;
+    protected String department;
+    protected String email;
+    protected String phoneNumber;
+    protected String yearOfJoin;
+    protected Date dob;
+    protected char gender;
+    protected College college;
+
+    public String getID() {return ID;}
+    public void setID(String ID) {this.ID = ID;}
 
     public String getName() {return name;}
     public String getDepartment() {return department;}
     public String getEmail() {return email;}
     public String getPhoneNumber() {return phoneNumber;}
     public String getYearOfJoin() {return yearOfJoin;}
-    public String getCollegeCode() {return collegeCode;}
+
     public Date getDob() {return dob;}
     public char getGender() {return gender;}
 
@@ -24,12 +38,36 @@ abstract class Person {
     public void setEmail(String email) {this.email = email;}
     public void setPhoneNumber(String phoneNumber) {this.phoneNumber = phoneNumber;}
     public void setYearOfJoin(String yearOfJoin) {this.yearOfJoin = yearOfJoin;}
-    public void setCollegeCode(String collegeCode) {this.collegeCode = collegeCode;}
+
     public void setDob(Date dob) {this.dob = dob;}
     public void setGender(char gender) {this.gender = gender;}
 }
 
 class Teacher extends Person{
+    private List<Course> courses;
+    private Map<String, Course> courseMap;
+
+    Teacher(){
+        courses = new ArrayList<>();
+        courseMap = new HashMap<>();
+    }
+
+    public Course makeCourse(String courseCode, String courseName, String section, int semester){
+        Course newCourse = new Course(courseCode, courseName);
+        newCourse.setTeacherID(this.ID);
+        newCourse.setCollegeID(this.college.getCollegeCode());
+        newCourse.setSection(section);
+        newCourse.setSemester(semester);
+        courses.add(newCourse);
+        courseMap.put(courseCode, newCourse);
+        return newCourse;
+    }
+    public boolean removeCourse(String courseCode){
+        if (!courseMap.containsKey(courseCode)){return false;}
+        courses.remove(courseMap.get(courseCode));
+        courseMap.remove(courseCode);
+        return true;
+    }
 
 }
 
